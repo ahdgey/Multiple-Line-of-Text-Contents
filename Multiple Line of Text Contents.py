cb64cdf20f083f9283260a3c6463018dda10448b
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-04-27 16:28:29 +0800

    Course and Section

--- Multiple Line of Text Contents.py
+++ Multiple Line of Text Contents.py
@@ -8,3 +8,4 @@ print("\033[1;97mA Method in Python to Write Multiple Line of Text into a Text F
 print("\033[0;36m*" * 90)
 
 print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
+print("\033[1;32m\nShe's from BSCPE 1-4")
