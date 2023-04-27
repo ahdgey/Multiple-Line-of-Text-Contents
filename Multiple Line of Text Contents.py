1874275cb3ba41c625e32b9bb20e65d9b17ecce8
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-04-27 16:25:42 +0800

    Title

--- Multiple Line of Text Contents.py
+++ Multiple Line of Text Contents.py
@@ -2,3 +2,8 @@
 #BSCPE 1-4
 #Multiple Line of Text Contents
 
+#Title
+print("\033[0;36m*" * 90)
+print("\033[1;97mA Method in Python to Write Multiple Line of Text into a Text File".center(97))
+print("\033[0;36m*" * 90)
+
